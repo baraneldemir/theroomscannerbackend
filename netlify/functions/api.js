97b04f2a32035d81d2b7b3cce3a884d813d0ffae
baraneldemir <brneldemirrr@gmail.com>
2024-10-18 22:16:27 +0100

    netlify chromium workaround commit 18 oct

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -40,7 +40,7 @@ api.use(cors({
 // await gotoWithRetry(page, searchURL);
 
 const scrapeImages = async (location) => {
-    const results = { images: [], links: [], description: [], prices: [], titles: [] };
+    const results = { images: [] };
 
     try {
         const browser = await puppeteer.launch({
@@ -81,26 +81,26 @@ const scrapeImages = async (location) => {
 
         const data = await page.evaluate(() => {
             const images = Array.from(document.querySelectorAll('figure img')).map(img => img.src);
-            const prices = Array.from(document.querySelectorAll('strong.listingPrice')).map(strong => strong.innerText.trim());
-            const titles = Array.from(document.querySelectorAll('em.shortDescription')).map(em => em.childNodes[0].textContent.trim());
-            const description = Array.from(document.querySelectorAll('p.description')).map(p => p.textContent.trim());
-            const links = Array.from(document.querySelectorAll('a[data-detail-url]')).map(a => a.getAttribute('href'));
+            // const prices = Array.from(document.querySelectorAll('strong.listingPrice')).map(strong => strong.innerText.trim());
+            // const titles = Array.from(document.querySelectorAll('em.shortDescription')).map(em => em.childNodes[0].textContent.trim());
+            // const description = Array.from(document.querySelectorAll('p.description')).map(p => p.textContent.trim());
+            // const links = Array.from(document.querySelectorAll('a[data-detail-url]')).map(a => a.getAttribute('href'));
 
             return images.map((image, index) => ({
                 image,
-                description: description[index] || 'No description',
-                price: prices[index] || 'N/A',
-                title: titles[index] || 'No Title',
-                link: links[index] || 'No link',
+                // description: description[index] || 'No description',
+                // price: prices[index] || 'N/A',
+                // title: titles[index] || 'No Title',
+                // link: links[index] || 'No link',
             }));
         });
 
         data.forEach(listing => {
             results.images.push(listing.image);
-            results.prices.push(listing.price);
-            results.titles.push(listing.title);
-            results.links.push(listing.link);
-            results.description.push(listing.description);
+            // results.prices.push(listing.price);
+            // results.titles.push(listing.title);
+            // results.links.push(listing.link);
+            // results.description.push(listing.description);
         });
 
         await browser.close();
