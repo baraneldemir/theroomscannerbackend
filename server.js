ed3898855617fabc6e0cf510d725b1d2bbe9ed89
baraneldemir <brneldemirrr@gmail.com>
2024-10-18 20:56:43 +0100

    netlify chromium workaround commit 18 oct

--- server.js
+++ server.js
@@ -13,17 +13,14 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 4000;
 
 const scrapeImages = async (location) => {
-    const results = { images: [], prices: [], titles: [], links: [], listingLocations: [], description: [] };
+    const results = { images: [], links: [], description: [], prices: [], titles: [] };
 
     try {
         const browser = await puppeteer.launch({
-            headless: false,
-            args: [
-                // '--no-sandbox',
-                // '--disable-setuid-sandbox',
-                // '--disable-dev-shm-usage',
-            ],
-            executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome', // Ensure this path is correct
+            headless: true,
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath,
         });
 
         const page = await browser.newPage();
@@ -32,15 +29,15 @@ const scrapeImages = async (location) => {
         const searchURL = `https://www.spareroom.co.uk/flatshare/${location}`;
         console.log(`Scraping: ${searchURL}`);
         
-        await page.goto(searchURL, { waitUntil: 'networkidle2', timeout: 500000 });
-        await page.waitForSelector('figure img', { visible: true, timeout: 500000 });
+        await page.goto(searchURL, { waitUntil: 'networkidle2', timeout: 0 });
+        await page.waitForSelector('figure img', { visible: true, timeout: 0 });
 
         const data = await page.evaluate(() => {
             const images = Array.from(document.querySelectorAll('figure img')).map(img => img.src);
             const prices = Array.from(document.querySelectorAll('strong.listingPrice')).map(strong => strong.innerText.trim());
-            const headers = Array.from(document.querySelectorAll('strong.listingPrice')).map(strong => strong.innerText.trim());
+            // const headers = Array.from(document.querySelectorAll('strong.listingPrice')).map(strong => strong.innerText.trim());
             const titles = Array.from(document.querySelectorAll('em.shortDescription')).map(em => em.childNodes[0].textContent.trim());
-            const listingLocations = Array.from(document.querySelectorAll('span.listingLocation')).map(span => span.textContent.trim());
+            // const listingLocations = Array.from(document.querySelectorAll('span.listingLocation')).map(span => span.textContent.trim());
             const description = Array.from(document.querySelectorAll('p.description')).map(p => p.textContent.trim());
             const links = Array.from(document.querySelectorAll('a[data-detail-url]')).map(a => a.getAttribute('href'));
 
@@ -50,8 +47,8 @@ const scrapeImages = async (location) => {
                 price: prices[index] || 'N/A',
                 title: titles[index] || 'No Title',
                 link: links[index] || 'no link',
-                headers: headers[index] || 'no headers',
-                listingLocations: listingLocations[index] || 'no listingLocations'
+                // headers: headers[index] || 'no headers',
+                // listingLocations: listingLocations[index] || 'no listingLocations'
             }));
         });
 
@@ -60,9 +57,9 @@ const scrapeImages = async (location) => {
             results.prices.push(listing.price);
             results.titles.push(listing.title);
             results.links.push(listing.link);
-            results.headers.push(listing.headers);
+            // results.headers.push(listing.headers);
             results.description.push(listing.description);
-            results.listingLocations.push(listing.listingLocations);
+            // results.listingLocations.push(listing.listingLocations);
         });
 
         await browser.close();
