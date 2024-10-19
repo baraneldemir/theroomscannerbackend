b7357268d0af71e0abcacb2899c08de748cf277a
baraneldemir <brneldemirrr@gmail.com>
2024-10-19 15:22:49 +0100

    puppeeter will not use again me -Wise Yoda

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -40,7 +40,7 @@ api.use(cors({
 // await gotoWithRetry(page, searchURL);
 
 const scrapeImages = async (location) => {
-    const results = { images: [] };
+    const results = { titles: [] };
     let browser;
 
     try {
@@ -85,22 +85,34 @@ const scrapeImages = async (location) => {
         await page.waitForSelector('figure img', { visible: true, timeout: 120000 });
 
         const data = await page.evaluate(() => {
-            const images = Array.from(document.querySelectorAll('figure img')).map(img => img.src);
-            return images.map(image => ({ image }));
+            // const images = Array.from(document.querySelectorAll('figure img')).map(img => img.src);
+            // const prices = Array.from(document.querySelectorAll('strong.listingPrice')).map(strong => strong.innerText.trim());
+            const titles = Array.from(document.querySelectorAll('em.shortDescription')).map(em => em.childNodes[0].textContent.trim());
+            // const description = Array.from(document.querySelectorAll('p.description')).map(p => p.textContent.trim());
+            // const links = Array.from(document.querySelectorAll('a[data-detail-url]')).map(a => a.getAttribute('href'));
+
+            return titles.map((title, index) => ({
+                // image,
+                // description: description[index] || 'no description',
+                // price: prices[index] || 'N/A',
+                title: titles[index] || 'No Title',
+                // link: links[index] || 'no link',
+            }));
         });
 
-        results.images.push(...data.map(listing => listing.image));
-        
+        data.forEach(listing => {
+            // results.images.push(listing.image);
+            // results.prices.push(listing.price);
+            results.titles.push(listing.title);
+            // results.links.push(listing.link);
+            // results.description.push(listing.description);
+        });
+        await browser.close();
+        return results;
     } catch (error) {
-        console.error('Error during scraping:', error.message);
-        throw new Error(`Failed to scrape images: ${error.message}`);
-    } finally {
-        if (browser) {
-            await browser.close();
-        }
+        console.error('Error scraping images:', error);
+        throw new Error('Failed to scrape images');
     }
-
-    return results;
 };
 
 
