021a7652fd65814e4b71eb2c2974a4a6e0d667a5
baraneldemir <brneldemirrr@gmail.com>
2024-10-20 11:43:32 +0100

    puppeteer-core removed

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const port = process.env.PORT || 4000;
 const scrapeImages = async (location) => {
     const results = { images: [] };
     const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
         headless: true,
     });
     const page = await browser.newPage()
