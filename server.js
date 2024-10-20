75e04e9bddb91ad48b6930025120907697b940fd
baraneldemir <brneldemirrr@gmail.com>
2024-10-20 14:19:53 +0100

    heroku fix

--- server.js
+++ server.js
@@ -15,12 +15,7 @@ const port = process.env.PORT || 4000;
 const scrapeImages = async (location) => {
     const results = { images: [], prices: [], titles: [], headers: [], description: [], links: [] };
     const browser = await puppeteer.launch({
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage', // This helps with shared memory issues
-            '--disable-gpu',            // Disable GPU hardware acceleration          
-        ],
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
         headless: true,
     });
     const page = await browser.newPage()
@@ -37,11 +32,12 @@ const scrapeImages = async (location) => {
         const titles = Array.from(document.querySelectorAll('em.shortDescription')).map(em => em.childNodes[0].textContent.trim());
         const headers = Array.from(document.querySelectorAll('a h2')).map(h2 => h2.textContent.trim());
         const description = Array.from(document.querySelectorAll('p.description')).map(p => p.textContent.trim());
+        // const links = Array.from(document.querySelectorAll('a[data-detail-url]')).map(a => a.getAttribute('href'));
         const links = Array.from(document.querySelectorAll('a[data-detail-url]')).map(a => {
             const relativeLink = a.getAttribute('href');
             return `https://www.spareroom.co.uk${relativeLink}`;
         });
-
+    
         return images.map((image, index) => ({
             image,
             description: description[index] || 'no description',
@@ -74,7 +70,7 @@ app.get('/scrape-images/:location', async (req, res) => {
         res.json(data);
     } catch (error) {
         console.error("Error scraping images:", error.message);
-        res.status(500).json({ error: "Failed to load rooms" });
+        res.status(500).json({ error: "Failed to scrape images" });
     }
 });
 
